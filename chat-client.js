7795ab7af71a769a10f45aa19a1f973396079bca
thomasmpeters <69325986+thomasmpeters@users.noreply.github.com>
2023-04-02 19:30:47 -0500

    V2

--- chat-client.js
+++ chat-client.js
@@ -4,12 +4,6 @@ const http = require("http"),
 
 const port = 3456;
 const file = "client.html";
-
-const chatrooms = {
-    room1: [],
-    room2: [],
-  };
-  
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html, on port 3456:
 const server = http.createServer(function (req, res) {
     // This callback runs when a new connection is made to our HTTP server.
@@ -28,6 +22,8 @@ server.listen(port);
 const socketio = require("socket.io")(http, {
     wsEngine: 'ws'
 });
+var globalMessages = {};
+
 
 // Attach our Socket.IO server to our HTTP server to listen
 const io = socketio.listen(server);
@@ -36,26 +32,33 @@ io.sockets.on("connection", function (socket) {
 
     socket.on('message_to_server', function (data) {
         // This callback runs when the server receives a new message from the client.
-        console.log("message: " + data["message"] + "chatroom #" + data["currentChatRoom"]); // log it to the Node.JS output
-        io.sockets.emit("message_to_client", { message: data["message"], user: data["user"]}) // broadcast the message to other users
+        console.log("message: " + data["message"]); // log it to the Node.JS output
+        const room = data["room"];
+        const message = { message: data["message"], user: data["user"], room: room };
+        if (!globalMessages[room]) {
+            globalMessages[room] = []; 
+          }
+        globalMessages[room].push(message);
+        console.log(globalMessages)
+        io.sockets.emit("message_to_client", { message: data["message"], user: data["user"], room: data['room'], globalMessages: globalMessages }) // broadcast the message to other users
     });
-
-
-    socket.on('newChatroom', function (data) {
-        console.log("room: " + data["newRoom"] + "user: "+ data['user']); // log it to the Node.JS output
-        io.sockets.emit("newChatroom", { newRoom: data["newRoom"] }) // broadcast the message to other users
-    });
-
-    socket.on('nickName', function (data) {
-        console.log("User NickName " + data["nickNames"]); // log it to the Node.JS output
-        io.sockets.emit("user_to_client", { nickNames: data["nickNames"] }) // broadcast the message to other users
-    });
-    socket.on('currentChatRoom', function (data) {
-        console.log(data["currentChatRoom"]); // log it to the Node.JS output
-        io.sockets.emit("user_to_client", { nickNames: data["nickNames"], currentChatRoom: data['currentChatRoom'] }) // broadcast the message to other users
-    });
-
     
+    socket.on('setCurrentUser', function (data) {
+        console.log("userwhologgedin: " + data["user"]); // log it to the Node.JS output
+        io.sockets.emit("set_user_to_client", { user: data["user"] }) // broadcast the message to other users
+
+    });
+
+    socket.on('setCurrentRoom', function (data) {
+        console.log("currentRoom: " + data["room"]); // log it to the Node.JS output
+        io.sockets.emit("set_room_to_client", { room: data["room"] }) // broadcast the message to other users
+    });
+
+    socket.on('createPublicChatRoom', function(data){
+        console.log("New room: " + data["room"]);
+        io.sockets.emit("new_public_room", {room: data["room"]})
+    });
+
 
 
 });
