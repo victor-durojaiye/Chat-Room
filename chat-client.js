b0dc25848c31a105085885b2537363e29eec2567
thomasmpeters <69325986+thomasmpeters@users.noreply.github.com>
2023-04-05 02:43:07 -0500

    V6

--- chat-client.js
+++ chat-client.js
@@ -27,6 +27,10 @@ var allUsers = [];
 var allRooms = [];
 var usersToRooms = {};
 
+var connectedUsers = {};
+
+var bannedUsers = {};
+
 
 // Attach our Socket.IO server to our HTTP server to listen
 const io = socketio.listen(server);
@@ -37,22 +41,33 @@ io.sockets.on("connection", function (socket) {
 
 
     socket.on('username_to_server', function (data) {
+      //here
       user = data['user'];
+      connectedUsers[data.user] = socket.id;
+      console.log(JSON.stringify(connectedUsers));
       if (!allUsers.includes(user)){
         allUsers.push(user);
-        io.sockets.emit("successfulLogin", { user: user })
+
+        var recipientSocketId = connectedUsers[data['user']];
+
+        io.to(recipientSocketId).emit('successfulLogin', {user:user})
 
       }
       else{
-        io.sockets.emit("loginError", { message: "Username Taken" }) // broadcast the login error
-      }
+        var recipientSocketId = connectedUsers[data['user']];
+
+        io.to(recipientSocketId).emit('loginError', {user:user})      }
 
     });
 
 
     socket.on('message_to_server', function (data) {
         // This callback runs when the server receives a new message from the client.
+        if (data['message'] == ":)"){
+          data['message'] = "&#128512";
+        }
         io.to(data['room']).emit('message_to_client', {message: data['message'], user: data['user'], room: data['room']})
+        
         // io.sockets.emit("message_to_client", { message: data["message"], user:data['user'], room: data['room']}) // broadcast the message to other users
     });
 
@@ -63,7 +78,8 @@ io.sockets.on("connection", function (socket) {
       var roomName = data['nameOfRoom'];
       usersToRooms[roomName] = {
         password: password,
-        user: [user]
+        user: [user],
+        bannedUsers: ""
       };
 
       if (!allRooms.includes(roomName)){
@@ -71,13 +87,57 @@ io.sockets.on("connection", function (socket) {
 
       }
       else{
-        io.sockets.emit("roomNameError", { message: "Roomname Taken" }) // broadcast the login error
-
+        var recipientSocketId = connectedUsers[user];
+        io.to(recipientSocketId).emit("roomNameError", { message: "Roomname Taken" })
       }
   });
 
   socket.on('get_rooms_from_server', function (data) {
-    io.sockets.emit("give_rooms_to_client", { allRooms: allRooms, usersToRooms: usersToRooms }) // send all rooms to client side
+    var user = data['user'];
+    var socketID = connectedUsers[user];
+
+    // io.sockets.emit("give_rooms_to_client", { allRooms: allRooms, usersToRooms: usersToRooms }) // send all rooms to client side
+    io.to(socketID).emit("give_rooms_to_client", { allRooms: allRooms, usersToRooms: usersToRooms})
+
+});
+
+socket.on('privateMessage', function(data){
+  var userTo = data['userTo'];
+  var recipientSocketId = connectedUsers[userTo];
+  console.log("private: " + JSON.stringify(data));
+  io.to(recipientSocketId).emit('message_to_user', {message: data['message'], user: data['user']})
+});
+
+socket.on('user_leave', function (data) {
+  let user = data['user'];
+  let room = data['roomToLeave'];
+  var recipientSocketId = connectedUsers[user];
+  socket.leave(room);
+  io.to(recipientSocketId).emit('userLeftRoom', { user: user, room: room})
+  
+  io.to(room).emit('give_rooms_to_client', {allRooms: allRooms, usersToRooms: usersToRooms})
+
+  io.to(room).emit('announceLeave', {room: room, user: user})
+
+
+});
+
+
+socket.on('banUser', function (data) {
+  let user = data['userTo'];
+  let room = data['room'];
+  usersToRooms[room] = {
+    bannedUsers: user
+  };
+  var socketID = connectedUsers[user];
+  io.to(socketID).emit('hideBan', {user:user})
+
+});
+
+socket.on('userIsTyping', function (data) {
+  var room = data['room'];
+  var user = data['user'];
+  io.to(room).emit('isTyping', { user: data['user']})
 });
 
 
@@ -86,24 +146,28 @@ socket.on('joinRoom', function (data) {
   let roomName = data['roomName'];
   let user = data['user'];
   let password = data['password'];
+  var recipientSocketId = connectedUsers[user];
+  if(usersToRooms[roomName].bannedUsers == user){
+    io.to(roomName).emit("userBannedFailure", { message: ""}); 
 
+  }
 
-  if (usersToRooms[roomName].password == ""){
+  else if (usersToRooms[roomName].password == ""){
     usersToRooms[roomName].user.push(user);
     socket.join(roomName);
     io.to(roomName).emit("userJoinedRoom", { user: user, room:roomName }); 
-    console.log(usersToRooms);
   }
 
   else if(password == usersToRooms[roomName].password){
 
     usersToRooms[roomName].user.push(user);
     socket.join(roomName);
-    io.to(roomName).emit("userJoinedRoom", { user: user, room:roomName }); 
+    io.to(recipientSocketId).emit("userJoinedRoom", { user: user, room:roomName }); 
     console.log(usersToRooms);
     
   } else {
-    // handle incorrect password
+    io.to(recipientSocketId).emit("wrongPassword", { user: user, room:roomName }); 
+
   }
 });
 });
